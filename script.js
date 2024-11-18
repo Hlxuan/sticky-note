3c98b0f9164c34e7e485128ebcc47509cbc9f003
Hlxuan <huanglangxuan@hlxuan.top>
2024-11-18 09:44:35 +0800

    update:动态创建便利贴结构

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ addBtn.addEventListener("click", function () {
 function addNewNote(text = "") {
   const note = document.createElement("div")
   note.classList.add("note")
+
+  note.innerHTML = `
+    <div class="note">
+        <div class="tools">
+            <button class="edit">
+                <i class="fas fa-edit"></i>
+            </button>
+            <button class="delete">
+                <fas class="fas fa-trash-alt"></fas>
+            </button>
+        </div>
+
+        <div class="main hidden"></div>
+        <textarea></textarea>
+    </div>
+  `
 }
